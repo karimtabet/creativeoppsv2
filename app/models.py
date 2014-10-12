26cbe8a3f4a898cf2ab80a23769f1e09438b10d6
unknown <ktabet@ktabet-W7-1.corp.adobe.com>
2014-10-12 13:53:07 +0100

    adds flask-openid and login to requirements

--- app/models.py
+++ app/models.py
@@ -7,3 +7,14 @@ class Admin(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.name)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(240))
+    description = db.Column(db.String(380))
+    location = db.Column(db.String(240))
+    body = db.Column(db.String(2048))
+    date = db.Column(db.Date)
+
+    def __repr__(self):
+        return '<Post %r>' % (self.body)
