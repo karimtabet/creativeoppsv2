67a0b39f060dada1ed1bccb3d336d17f48c690cf
unknown <ktabet@ktabet-W7-1.corp.adobe.com>
2014-10-12 14:22:51 +0100

    configures login and openid endpoints

--- app/models.py
+++ app/models.py
@@ -2,11 +2,26 @@ from app import db
 
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
 
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2
+        except NameError:
+            return str(self.id)  # python 3
+
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<User %r>' % (self.nickname)
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key = True)
