bb6e7ab3dbcc5ddf105aee65c5fe7eef59bb7d21
unknown <ktabet@ktabet-W7-1.corp.adobe.com>
2014-11-22 18:18:13 +0000

    cleans up config

--- config.py
+++ config.py
@@ -8,8 +8,4 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
-
-ADMIN_USERS = [
-    {'karimtabet@gmail.com'}]
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'}
