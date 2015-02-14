32fa1888993828d11227758583709abcf8d0447c
kreamt <karimtabet@gmail.com>
2015-02-14 16:10:46 +0000

    rewrites admin and login views. pep8 formatting.

--- app/views.py
+++ app/views.py
@@ -1,149 +1,116 @@
-from flask import render_template, flash, redirect, session, url_for, request, g
-from flask.ext.login import login_user, logout_user, current_user, login_required
-from app import app, db, lm, oid
-from forms import ProjectForm
-from models import Admin, Project, Picture, Video
-import urllib2, urlparse, json
+import urllib2
+import urlparse
+import json
+from flask import render_template, redirect, url_for, request
+from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext.login import login_user, logout_user, current_user
+from passlib.hash import sha256_crypt
+from app import app, db, admin, login_manager
+from models import Project, Picture, Video, Admin
+
 
 @app.route('/')
 def index():
     projects = Project.query.all()
     return render_template('index.html',
-                            projects=projects)
+                           projects=projects)
+
 
 @app.route('/project/<project_id>', methods=['GET'])
 def project(project_id):
-  project = Project.query.filter_by(id=project_id).first()
-  pictures = Picture.query.filter_by(project_id=project_id)
-  videos = Video.query.filter_by(project_id=project_id)
-  return render_template('project.html',
-                            project=project,
-                            pictures=pictures,
-                            videos=videos)
+    project = Project.query.filter_by(id=project_id).first()
+    pictures = Picture.query.filter_by(project_id=project_id)
+    videos = Video.query.filter_by(project_id=project_id)
+    return render_template('project.html',
+                           project=project,
+                           pictures=pictures,
+                           videos=videos)
 
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-    if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('admin'))
-    return oid.try_login('https://www.google.com/accounts/o8/id')
-
-@lm.user_loader
-def load_user(id):
-    return Admin.query.get(int(id))
-
-@oid.after_login
-def after_login(resp):
-    if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
-        return redirect(url_for('login'))
-    admin = Admin.query.filter_by(email=resp.email).first()
-    if admin is None:
-        flash('Admin does not exist')
-        return redirect(url_for('login'))
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(admin, remember = remember_me)
-    return redirect(url_for('admin'))
-
-@app.before_request
-def before_request():
-    g.user = current_user
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/admin')
-@login_required
-def admin():
-    projects = Project.query.all()
-    return render_template('admin.html',
-                           projects=projects)
-
-@app.route('/admin/project', defaults = {'project_id':None},
-          methods=['GET', 'POST'])
-@app.route('/admin/project/<project_id>',
-          methods=['GET', 'POST'])
-@login_required
-def update_project(project_id):
-    form = ProjectForm()
-    if form.validate_on_submit():
-      project = Project(title=form.title.data,
-                        description=form.description.data,
-                        location=form.location.data,
-                        body=form.body.data,
-                        date=form.date.data,
-                        album_url=form.album_url.data,
-                        thumbnail_url=form.thumbnail_url.data,
-                        video_urls=form.video_urls.data)
-      db.session.add(project)
-      db.session.commit()
-      db.session.refresh(project)
-      if project.album_url:
-        album_id = project.album_url[project.album_url.find('sets/')+5:-1]
-        get_pictures(album_id, project.id)
-      if project.video_urls:
-        get_videos(project.video_urls, project.id)
-      return redirect(url_for('admin'))
-    return render_template('project_form.html',
-                           form=form)
-
-@app.route('/admin/project/delete/<project_id>',
-          methods=['GET', 'POST'])
-@login_required
-def deleteProject(project_id):
-    if request.method == 'POST':
-      project = Project.query.filter_by(id=project_id).first()
-      db.session.delete(project)
-
-      pictures = Picture.query.filter_by(project_id=project_id).all()
-      for pic in pictures:
-        db.session.delete(pic)
-
-      db.session.commit()
-      return redirect(url_for('admin'))
 
 def get_pictures(album_id, project_id):
     thumbnail_url = ''
     picture_url = ''
     last_picture = ''
-    response = urllib2.urlopen("https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos" + 
-                                "&api_key=28b8b728bfb3dda2af61f99f29d98334&photoset_id=" + album_id + 
-                                "&format=json&nojsoncallback=1").read()
+    response = urllib2.urlopen(
+        "https://api.flickr.com/services/rest/" +
+        "?method=flickr.photosets.getPhotos" +
+        "&api_key=28b8b728bfb3dda2af61f99f29d98334&photoset_id=" +
+        album_id + "&format=json&nojsoncallback=1"
+      ).read()
     response_json = json.loads(response)
     for line in response_json['photoset']['photo']:
-      photo_id = line['id']
-      response = urllib2.urlopen("https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&" +
-                                  "api_key=28b8b728bfb3dda2af61f99f29d98334&photo_id=" + photo_id +
-                                  "&format=json&nojsoncallback=1").read()
-      response_json = json.loads(response)
-      for line in response_json['sizes']['size']:
-        if line['label'] == 'Medium':
-          thumbnail_url = line['source']
-        if line['label'] == 'Original':
-          picture_url = line['source']
+        photo_id = line['id']
+        response = urllib2.urlopen(
+            "https://api.flickr.com/services/rest/" +
+            "?method=flickr.photos.getSizes&" +
+            "api_key=28b8b728bfb3dda2af61f99f29d98334&photo_id=" + photo_id +
+            "&format=json&nojsoncallback=1").read()
+        response_json = json.loads(response)
+        for line in response_json['sizes']['size']:
+            if line['label'] == 'Medium':
+                thumbnail_url = line['source']
+            if line['label'] == 'Original':
+                picture_url = line['source']
 
-        if picture_url and picture_url != last_picture:
-          last_picture = picture_url
-          picture = Picture(thumbnail_url=thumbnail_url,
-                          image_url=picture_url,
-                          project_id=project_id)
-          db.session.add(picture)
+            if picture_url and picture_url != last_picture:
+                last_picture = picture_url
+                picture = Picture(thumbnail_url=thumbnail_url,
+                                  image_url=picture_url,
+                                  project_id=project_id)
+            db.session.add(picture)
     db.session.commit()
+
 
 def get_videos(video_urls, project_id):
-  for video_url in video_urls.split(','):
-    video_url = video_url.strip()
-    url_data = urlparse.urlparse(video_url)
-    query = urlparse.parse_qs(url_data.query)
-    video_id = query["v"][0]
-    thumbnail_url = 'http://img.youtube.com/vi/' + video_id + '/default.jpg'
-    video = Video(video_url=video_url,
-                        thumbnail_url=thumbnail_url,
-                        project_id=project_id)
-    db.session.add(video)
-    db.session.commit()
+    for video_url in video_urls.split(','):
+        video_url = video_url.strip()
+        url_data = urlparse.urlparse(video_url)
+        query = urlparse.parse_qs(url_data.query)
+        video_id = query["v"][0]
+        thumbnail_url = (
+            'http://img.youtube.com/vi/' +
+            video_id + '/default.jpg'
+        )
+        video = Video(video_url=video_url,
+                      thumbnail_url=thumbnail_url,
+                      project_id=project_id)
+        db.session.add(video)
+        db.session.commit()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Admin.query.get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        user = Admin.query.filter_by(
+          username=request.form["username"]).first()
+        if user:
+            if sha256_crypt.verify(request.form['password'], user.password):
+                login_user(user)
+                return redirect(url_for('model_view_project.index_view'))
+            else:
+                error = 'Invalid Credentials. Please try again.'
+        else:
+            error = 'Invalid Credentials. Please try again.'
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+class AdminView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated()
+
+    def __init__(self, session, **kwargs):
+        super(AdminView, self).__init__(Project, session, **kwargs)
+
+admin.add_view(AdminView(db.session, endpoint="model_view_project"))
