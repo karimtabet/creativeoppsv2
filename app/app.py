429992843593215bd4d2b986aee68f0245853361
Karim Tabet <karim@karimtabet.com>
2015-10-11 21:57:41 +0100

    app configuration happening in own file, outside of init file

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask.ext.admin import Admin
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from config import config
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'postgresql+psycopg2://{role}:{password}@{host}:{port}/{db}'
+    .format(
+        **config['postgresql']
+    )
+)
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 3
+
+admin = Admin(app)
+db = SQLAlchemy(app)
