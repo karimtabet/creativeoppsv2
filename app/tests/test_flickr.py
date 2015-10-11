4be6934ac2bbe5a091a0909c72349273e17c8c9e
Karim Tabet <karim@karimtabet.com>
2015-10-11 22:28:56 +0100

    querying image not picture

--- app/tests/test_flickr.py
+++ app/tests/test_flickr.py
@@ -1,19 +1,21 @@
 from hamcrest import assert_that, is_
 
-from app import db
+from app.app import db
 from app.flickr import get_pictures
-from app.models import Picture
+from app.models import Image
 from app.tests.utils import CreativeOpportunitiesTestCase
 
 
 class TestFlickr(CreativeOpportunitiesTestCase):
     def test_get_pictures(self):
+        project = self.insert_n_projects(1)[0]
+
         get_pictures(
             'https://www.flickr.com/photos/128639640@N03/'
             'sets/72157648806036881/',
-            1
+            project.project_uuid
         )
-        pictures = db.session.query(Picture).all()
+        pictures = db.session.query(Image).all()
 
         assert_that(
             len(pictures),
