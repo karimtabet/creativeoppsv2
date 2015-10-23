491b7fd135b43a531f241473af98af6d8e1553f8
Karim Tabet <karim@karimtabet.com>
2015-10-23 12:32:06 +0100

    amend get youtube videos function to use project uuid and writes passing test for get 1 video

---
+++ app/tests/test_youtube.py
@@ -0,0 +1,32 @@
+from hamcrest import assert_that, is_, has_length
+
+from app.app import db
+from app.youtube import get_videos
+from app.models import Project
+from app.tests.utils import CreativeOpportunitiesTestCase
+
+
+class TestYoutube(CreativeOpportunitiesTestCase):
+    def test_get_video(self):
+        project = self.insert_n_projects(1)[0]
+
+        get_videos(
+            'https://www.youtube.com/watch?v=Lbjru5CQIW4',
+            project.project_uuid
+        )
+        project = db.session.query(Project).first()
+
+        assert_that(
+            project.videos,
+            has_length(1)
+        )
+
+        assert_that(
+            project.videos[0].video_url,
+            is_('https://www.youtube.com/watch?v=Lbjru5CQIW4')
+        )
+
+        assert_that(
+            project.videos[0].thumbnail_url,
+            is_('http://img.youtube.com/vi/Lbjru5CQIW4/default.jpg')
+        )
--- app/youtube.py
+++ app/youtube.py
@@ -4,18 +4,18 @@ from app.app import db
 from app.models import Video
 
 
-def get_videos(video_urls, project_id):
+def get_videos(video_urls, project_uuid):
     for video_url in video_urls.split(','):
         video_url = video_url.strip()
         url_data = parse.urlparse(video_url)
         query = parse.parse_qs(url_data.query)
         video_id = query["v"][0]
         thumbnail_url = (
-            'http://img.youtube.com/vi/{video_id}' +
-            video_id + '/default.jpg'
+            'http://img.youtube.com/vi/{video_id}/default.jpg'
+            .format(video_id=video_id)
         )
         video = Video(video_url=video_url,
                       thumbnail_url=thumbnail_url,
-                      project_id=project_id)
+                      project_uuid=project_uuid)
         db.session.add(video)
         db.session.commit()
