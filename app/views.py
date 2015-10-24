6504a6541570aff9c0817bb96b0e546c32946dce
Karim Tabet <karim@karimtabet.com>
2015-10-24 14:47:41 +0100

    reorders project model view and uses custom cktestareafield

--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
 from flask import render_template
 from flask.ext.admin.contrib.sqla import ModelView
+from wtforms import TextAreaField
+from wtforms.widgets import TextArea
 
 from app.app import app, db, admin
 from app.models import Project, Image, Video
-from app.flickr import get_pictures as get_flickr_pictures
-from app.youtube import get_videos as get_youtube_videos
 
 
 @app.route('/')
@@ -25,15 +25,37 @@ def project(project_id):
                            videos=videos)
 
 
-class AdminView(ModelView):
+class CKTextAreaWidget(TextArea):
+    def __call__(self, field, **kwargs):
+        if kwargs.get('class'):
+            kwargs['class'] += ' ckeditor'
+        else:
+            kwargs.setdefault('class', 'ckeditor')
+        return super(CKTextAreaWidget, self).__call__(field, **kwargs)
+
+
+class CKTextAreaField(TextAreaField):
+    widget = CKTextAreaWidget()
+
+
+class ProjectModelView(ModelView):
+    form_columns = [
+      'title',
+      'avatar_url',
+      'location',
+      'datetime',
+      'description',
+      'body',
+      'images',
+      'videos'
+    ]
+    form_overrides = {
+        'body': CKTextAreaField
+    }
+
     def on_model_change(self, form, model, is_created):
-        if model.album_url:
-            album_id = model.album_url[model.album_url.find('sets/')+5:-1]
-            get_flickr_pictures(album_id, model.id)
-        if model.video_urls:
-            get_youtube_videos(model.video_urls, model.id)
+        model.id = form.title.data.lower().replace(' ', '-')
 
-    def __init__(self, session, **kwargs):
-        super(AdminView, self).__init__(Project, session, **kwargs)
-
-admin.add_view(AdminView(db.session, endpoint='model_view_project'))
+admin.add_view(
+  ProjectModelView(Project, db.session, endpoint='project_model_view')
+)
