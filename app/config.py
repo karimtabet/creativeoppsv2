30f76590f73e47eaa48785b86e740557a6348a38
Karim Tabet <karim@karimtabet.com>
2015-11-07 15:23:33 +0000

    config coming from env variables

--- app/config.py
+++ app/config.py
@@ -1,30 +1,15 @@
-import yaml
-from voluptuous import Schema, Required, MultipleInvalid
-from logging import getLogger, basicConfig
+import os
 
-logger = getLogger(__name__)
 
-config_schema = Schema({
-    Required("postgresql"): {
-        Required("role"): str,
-        Required("password"): str,
-        Required("host"): str,
-        Required("port"): int,
-        Required("db"): str
-    },
-    Required("flickr_api_key"): str
-})
-
-with open("/opt/creativeopportunities.yml") as configuration_file:
-    try:
-        config = yaml.safe_load(configuration_file)
-        config_schema(config)
-        logger.info("config loaded - valid")
-    except IOError:
-        basicConfig()
-        logger.critical("unable to open configuration file")
-        exit(1)
-    except MultipleInvalid as e:
-        basicConfig()
-        logger.critical("invalid config file: {e}".format(e=e))
-        exit(1)
+class BaseConfig(object):
+    SECRET_KEY = os.environ['SECRET_KEY']
+    DEBUG = os.environ['DEBUG']
+    DB_NAME = os.environ['DB_NAME']
+    DB_USER = os.environ['DB_USER']
+    DB_PASS = os.environ['DB_PASS']
+    DB_SERVICE = os.environ['DB_SERVICE']
+    DB_PORT = os.environ['DB_PORT']
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
+        DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
+    )
+    FLICKR_API_KEY = os.environ["FLICKR_API_KEY"]
