49d1b5691b15dd3103ffada75ab75fee1d4c06be
Karim Tabet <karim@karimtabet.com>
2015-11-07 15:23:43 +0000

    update location of app.config

--- app/app.py
+++ app/app.py
@@ -2,17 +2,10 @@ from flask import Flask
 from flask.ext.admin import Admin
 from flask.ext.sqlalchemy import SQLAlchemy
 
-from app.config import config
+from app.config import BaseConfig
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'postgresql+psycopg2://{role}:{password}@{host}:{port}/{db}'
-    .format(
-        **config['postgresql']
-    )
-)
-app.config['SQLALCHEMY_POOL_TIMEOUT'] = 3
-app.config['SECRET_KEY'] = 'very_secret_key'
+app.config.from_object(BaseConfig)
 
 admin = Admin(app)
 db = SQLAlchemy(app)
