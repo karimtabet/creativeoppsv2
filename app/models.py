835c30a67fae9c1709725b9b2ee19dc4b9b384ac
Karim Tabet <karimtabet@gmail.com>
2016-04-07 18:54:50 +0100

    project, images, videos as_dict, project description and body to markdown

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import MetaData, Column, String, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from markdown import markdown
 
 convention = {
     'ix': 'ix_%(column_0_label)s',
@@ -39,6 +40,19 @@ class Project(Base):
     def __str__(self):
         return '{title}'.format(title=self.title)
 
+    def as_dict(self):
+        d = {'title': self.title,
+             'description': markdown(self.description),
+             'location': self.location,
+             'body': markdown(self.body),
+             'datetime': self.datetime,
+             'avatar_url': self.avatar_url}
+        if len(self.images) > 0:
+            d['images'] = [image.as_dict() for image in self.images]
+        if len(self.images) > 0:
+            d['videos'] = [video.as_dict() for video in self.videos]
+        return d
+
 
 class Image(Base):
     __tablename__ = 'images'
@@ -54,6 +68,11 @@ class Image(Base):
     def __repr__(self):
         return '<Image {url}>'.format(url=self.image_url)
 
+    def as_dict(self):
+        return {'image_url': self.image_url,
+                'project_id': self.project_id,
+                'thumbnail_url': self.thumbnail_url}
+
 
 class Video(Base):
     __tablename__ = 'videos'
@@ -68,6 +87,11 @@ class Video(Base):
 
     def __repr__(self):
         return '<Video {url}>'.format(url=self.video_url)
+
+    def as_dict(self):
+        return {'video_url': self.video_url,
+                'project_id': self.project_id,
+                'thumbnail_url': self.thumbnail_url}
 
 
 class IndexCarouselItem(Base):
