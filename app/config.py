0e7b9bf85e90a0efda2ba45853efcf1cbc0b1f0f
Karim Tabet <karimtabet@gmail.com>
2016-04-18 20:05:06 +0100

    mail config

--- app/config.py
+++ app/config.py
@@ -4,4 +4,10 @@ import os
 class BaseConfig(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLICKR_API_KEY = os.environ["FLICKR_API_KEY"]
